a89a42169ed077cd7dd4b2fe5eb479ae139ffa2f
Fernando Mateus de Oliveira <mateusjp15@gmail.com>
2020-07-15 20:56:28 -0400

    Trabalho Pratico

--- app.js
+++ app.js
@@ -1,29 +1,51 @@
-const express         =     require('express')
-  , passport          =     require('passport')
-  , cookieParser      =     require('cookie-parser')
-  , session           =     require('express-session')
-  , bodyParser        =     require('body-parser')
-  , config            =     require('./configuration/config')
-  , app               =     express();
+const express = require('express'),
+  passport = require('passport'),
+  cookieParser = require('cookie-parser'),
+  session = require('express-session'),
+  bodyParser = require('body-parser'),
+  config = require('./configuration/config'),
+  facebookConfig = require('./configuration/facebook-config'),
+  app = express();
 
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var GoogleStrategy = require('passport-google-oauth20').Strategy,
+  FacebookStrategy = require('passport-facebook').Strategy;
 
-passport.use(new GoogleStrategy({
-    clientID: config.api_key,
-    clientSecret: config.api_secret,
-    callbackURL: config.callback_url
-  },
-  function(accessToken, refreshToken, profile, done) {
-    return done(null, profile);
-  }
-));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: config.api_key,
+      clientSecret: config.api_secret,
+      callbackURL: config.callback_url,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: facebookConfig.api_key,
+      clientSecret: facebookConfig.api_secret,
+      callbackURL: facebookConfig.callback_url,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      console.log(`AccessToken ${accessToken}`);
+      console.log(`RefreshToken ${refreshToken}`);
+      console.log(`Profile ${profile}`);
+
+      return done(null, profile);
+    }
+  )
+);
 
 // Passport session setup.
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
   done(null, user);
 });
 
-passport.deserializeUser(function(obj, done) {
+passport.deserializeUser(function (obj, done) {
   done(null, obj);
 });
 
@@ -31,25 +53,46 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(
+  session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.render('index', { user: req.user });
 });
 
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
-
-app.get('/auth/google/callback', 
-  passport.authenticate('google',  { successRedirect : '/', failureRedirect: '/login' }),
-  function(req, res) {
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+);
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+  }),
+  function (req, res) {
     res.redirect('/');
   }
 );
 
-app.get('/logout', function(req, res){
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get(
+  '/auth/facebook/callback',
+  passport.authenticate('facebook', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+  }),
+  function (req, res) {
+    res.redirect('/');
+  }
+);
+
+app.get('/logout', function (req, res) {
   req.logout();
   res.redirect('/');
 });
---
+++ configuration/facebook-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  api_key: 'api_key',
+  api_secret: 'api_secret',
+  callback_url: 'http://localhost:3000/auth/facebook/callback',
+};
